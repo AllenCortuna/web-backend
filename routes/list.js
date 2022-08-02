acc6184c16c38950fce55765ab6404695b037acf
zan <olsencortuna@gmail.com>
2022-08-03 07:25:42 +0800

    auth implemented

--- routes/list.js
+++ routes/list.js
@@ -2,14 +2,14 @@ import express from 'express';
 import { getLists,getList,getOwnLists,createList,updateList,deleteList} from '../controlers/list.js';
 
 const router = express.Router();
-// import auth from '../middleware/auth.js'
+import auth from '../middleware/auth.js'
 
 
 router.get('/', getLists);
-router.get('/', getOwnLists);
-router.post('/', createList);
-router.get('/:id', getList);
-router.patch('/:id', updateList);
-router.delete('/:id', deleteList);
+router.get('/',auth, getOwnLists);
+router.post('/',auth, createList);
+router.get('/:id',auth, getList);
+router.patch('/:id',auth, updateList);
+router.delete('/:id',auth, deleteList);
  
 export default router;
