627af41a4a80c037c2d16578c1dc618eac869c5c
zan <olsencortuna@gmail.com>
2022-08-02 10:55:24 +0800

    getall fix controllers

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 
-import {getUsers,signin , signup} from '../controlers/user.js';
+import {signin , signup} from '../controlers/user.js';
 
 
-router.get('/', getUsers);
 router.post('/signin', signin);
 router.post('/signup', signup);
 
