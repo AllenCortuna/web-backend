7626ec7ce816b382f960d0df12db3a952a4bd056
zan <olsencortuna@gmail.com>
2022-08-02 14:33:26 +0800

    model cleanup

--- controlers/user.js
+++ controlers/user.js
@@ -1,56 +1,47 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-import UserModal from '../models/user.js';
-const secret = 'test';
+import UserModal from "../models/user.js";
+const secret = "test";
 
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const oldUser = await UserModal.findOne({ email });
+    if (!oldUser)
+      return res.status(404).json({ message: "User does not exist " });
 
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-export const signin = async (req, res ) => {
-    const { email, password } = req.body;
+    if (!isPasswordCorrect)
+      return res.status(404).json({ message: "Invalid password" });
 
-    try {
-        const oldUser = await UserModal.findOne({email});
-        if (!oldUser) return res.status(404).json({message: 'User does not exist '});
-
-        const isPasswordCorrect = await bcrypt.compare(password ,oldUser.password);
-
-        if (!isPasswordCorrect) return res.status(404).json({message: 'Invalid password'});
- 
-        const token = jwt.sign({email: oldUser.email, id: oldUser._id},secret,{expiresIn: '1h'});
-        console.log("login ok");
-        res.status(200).json({result: oldUser,token});
-    } catch (err) {
-        res.status(500).json({message: 'Something went wrong '});
-    }
-}
-
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: "1w",
+    });
+    console.log("login ok");
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong " });
+  }
+};
 
 export const signup = async (req, res) => {
-    const {hotelName, email,location, password,contact,image} = req.body;
+  const { email, password } = req.body;
+  try {
+    const oldUser = await UserModal.findOne({ email });
+    if (oldUser) return res.status(400).json({ message: "User already exist" });
 
-    try {
-        const oldUser =  await UserModal.findOne({email});
-        if (oldUser)  return res.status(400).json({message: 'User already exist'});
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await UserModal.create({ email, password: hashedPassword });
 
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const result = await UserModal.create({email,password: hashedPassword, hotelName,location,contact,image});
-
-        const token = jwt.sign({email: result.email,id : result._id },secret, {expiresIn: '1h'})
-        res.status(201).json({result,token});
-    } catch (error) {
-        res.status(500).json({message: 'Something went wrong '});
-        console.log(error);
-    }
-}
-
-// export const getUsers = async (req, res) => { 
-//     try {
-//         const userModal = await UserModal.find().sort({_id: -1})
-//         console.log('getuser ok');
-//         res.status(200).json(userModal);
-//     } catch (error) {
-//         res.status(404).json({ message: error.message });
-//     }
-// }
+    const token = jwt.sign({ email: result.email, id: result._id }, secret, {
+      expiresIn: "1w",
+    });
+    res.status(201).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong " });
+    console.log(error);
+  }
+};
--- models/user.js
+++ models/user.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    hotelName: { type: String, required:  true },
-    location: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    contact: { type: String, required: true },
-    image: { type: String, required: true },
     id: { type: String },
 });
 
